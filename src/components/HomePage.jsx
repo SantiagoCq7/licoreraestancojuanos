0bb5f3d9762d1d5892fcfde85baca589e8a68cad
Santiago Cq <castilloquinterosantiago2005@gmail.com>
2025-04-11 00:01:52 -0500

    modificacion de la navbar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,19 +4,19 @@ export default function HomePage() {
 return (
     <div className="min-h-screen bg-black text-white font-sans">
         {/* Navbar */}
-        <nav className="flex justify-between items-center px-8 py-4 border-b border-gray-700">
+        <nav className="flex justify-between items-center px-8 py-4">
             <div className="flex items-center space-x-4">
                 <img
                     src="public/images/logo-nav.png" 
                     alt="Logo"
-                    className="h-20 w-50 object-contain"
+                    className="h-20 w-50 object-contain" 
                 />
             </div>
             <div className="flex items-center space-x-6 font-bold">
                 <a href="#productos" className="hover:text-yellow-400">PRODUCTOS</a>
                 <a href="#nosotros" className="hover:text-yellow-400">NOSOTROS</a>
                 <a href="#servicios" className="hover:text-yellow-400">SERVICIOS</a>
-                <a href="#contacto" className="bg-white text-black rounded-full px-4 py-1 hover:bg-yellow-400 transition">CONTÁCTANOS</a>
+                <a href="#contacto" className="border-2 text-white rounded-full px-4 py-1 hover:bg-white hover:text-black transition">CONTÁCTANOS</a>
                 <button className="bg-white text-2xl px-2 py-2 rounded-full hover:bg-yellow-400 transition">🛒</button>
             </div>
         </nav>
@@ -26,7 +26,7 @@ return (
             <img
                 src="public/images/bg-homepage.png"
                 alt="cheers"
-                className="absolute w-full h-full object-cover opacity-70"
+                className="absolute w-full h-full object-cover opacity-35"
             />
             <div className="relative z-10 text-center px-4">
                 <h1 className="text-6xl md:text-8xl font-extrabold leading-tight hover:text-yellow-400 transition">
